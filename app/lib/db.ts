1d113b9b56480791e845e0dea609984f5f1b1b51
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-08-17 22:56:40 +0530

    add database connection

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,17 +1,15 @@
-import { Pool } from "pg";
+import postgres from 'postgres';
 
-let pool: Pool;
-
-if (!(global as any).pgPool) {
-  (global as any).pgPool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-    max: 5,
-    idleTimeoutMillis: 10000,
-    connectionTimeoutMillis: 5000,
-  });
+declare global {
+  var __db: postgres.Sql | undefined;
 }
 
-pool = (global as any).pgPool;
+const db = global.__db || postgres(process.env.DATABASE_URL!, {
+  ssl: { rejectUnauthorized: false },
+});
 
-export default pool;
+if (process.env.NODE_ENV !== 'production') {
+  global.__db = db;
+}
+
+export default db;
